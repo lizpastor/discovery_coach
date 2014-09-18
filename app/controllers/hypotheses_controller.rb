00ea54122b770040bf6ab2fa095ba4458d6f9126
unknown <lharvey@LLS-LHARVEY.cb.careerbuilder.com>
2014-09-18 14:22:01 -0400

    coach pic

--- app/controllers/hypotheses_controller.rb
+++ app/controllers/hypotheses_controller.rb
@@ -1,4 +1,5 @@
 class HypothesesController < ApplicationController
+     before_action :authenticate_user!
   before_action :set_hypothesis, only: [:show, :edit, :update, :destroy]
 
   # GET /hypotheses
